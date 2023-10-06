98be79b60ef7d2f3cc731bbf34ed0e1b8911b814
psmacabulos <107623965+psmacabulos@users.noreply.github.com>
2023-10-06 19:37:45 +0800

    adjusted source to solve loading problem

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,23 +1,21 @@
 /* eslint-disable react/no-unknown-property */
-import { easing } from "maath";
-import { useSnapshot } from "valtio";
-import { useFrame } from "@react-three/fiber";
-import { Decal, useGLTF, useTexture } from "@react-three/drei";
+import { easing } from "maath"
+import { useSnapshot } from "valtio"
+import { useFrame } from "@react-three/fiber"
+import { Decal, useGLTF, useTexture } from "@react-three/drei"
 
-import state from "../store";
+import state from "../store"
 
 const Shirt = () => {
-  const snap = useSnapshot(state);
-  const { nodes, materials } = useGLTF("/shirt_baked.glb");
+  const snap = useSnapshot(state)
+  const { nodes, materials } = useGLTF("./shirt_baked.glb")
 
-  const logoTexture = useTexture(snap.logoDecal);
-  const fullTexture = useTexture(snap.fullDecal);
+  const logoTexture = useTexture(snap.logoDecal)
+  const fullTexture = useTexture(snap.fullDecal)
 
-  useFrame((state, delta) =>
-    easing.dampC(materials.lambert1.color, snap.color, 0.25, delta)
-  );
+  useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta))
 
-  const stateString = JSON.stringify(snap);
+  const stateString = JSON.stringify(snap)
   return (
     <group key={stateString}>
       <mesh
@@ -28,12 +26,7 @@ const Shirt = () => {
         dispose={null}
       >
         {snap.isFullTexture && (
-          <Decal
-            position={[0, 0, 0]}
-            rotation={[0, 0, 0]}
-            scale={1}
-            map={fullTexture}
-          />
+          <Decal position={[0, 0, 0]} rotation={[0, 0, 0]} scale={1} map={fullTexture} />
         )}
         {snap.isLogoTexture && (
           <Decal
@@ -48,7 +41,7 @@ const Shirt = () => {
         )}
       </mesh>
     </group>
-  );
-};
+  )
+}
 
-export default Shirt;
+export default Shirt
